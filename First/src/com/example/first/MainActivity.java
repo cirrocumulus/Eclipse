152e4b0d12a69ab1680817a37f6081f7c7188217
Lightning <Lightning@172.30.35.200>
2013-09-28 17:44:17 +0900

    Test_First

---
+++ First/src/com/example/first/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.first;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        TextView MyText = new TextView(this);
+        MyText.setText("코드로 문자열 출력하기");
+        setContentView(MyText);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+}
